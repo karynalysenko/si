ac48c5c82a40a38102ece961de8df7daa919913c
karynalysenko <karinalysenko@gmail.com>
2023-10-06 22:06:57 +0100

    exercicio 4 feito (aula 3)

---
+++ src/si/statistics/manhattan_distance.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def manhattan_distance(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    return np.abs(x - y).sum(axis=1)
+
+
+if __name__ == '__main__':
+    # test euclidean_distance
+    x = np.array([1, 2, 3])
+    y = np.array([[1, 2, 3], [4, 5, 6]])
+    our_distance = manhattan_distance(x, y)
+
+
+    from sklearn.metrics.pairwise import manhattan_distances
+    sklearn_distance = manhattan_distances(x.reshape(1, -1), y)
+    assert np.allclose(our_distance, sklearn_distance)
+    print(our_distance, sklearn_distance)
